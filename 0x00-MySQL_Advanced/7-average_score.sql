6fb2f518278aeef29ed28c66ca41111dc92783c5
OteIan <oteian9@gmail.com>
2024-03-24 19:03:10 +0300

    Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,21 @@
+-- Creates a stored procedure
+-- Computes and stores the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg FLOAT DEFAULT 0;
+
+    SELECT AVG(score) INTO avg
+        FROM projects
+        WHERE user_id=user_id;
+    
+    UPDATE users
+        SET average_score=avg
+        WHERE id=user_id;
+
+END $$
+DELIMITER ;
