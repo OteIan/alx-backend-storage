d0b0ed46dddd4a13006e69f98011ab179c8fa403
OteIan <oteian9@gmail.com>
2024-03-24 14:32:44 +0300

    ?

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,9 +2,6 @@
 SELECT
     band_name,
     (IFNULL(split, 2022) - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    FIND_IN_SET('Glam rock' IFNULL(style, "")) > 0
-ORDER BY
-    lifespan DESC;
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock' IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
