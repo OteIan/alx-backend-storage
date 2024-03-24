b0a2fef9d6b8f4ef80ea54fac8e4719ccadb3619
OteIan <oteian9@gmail.com>
2024-03-24 18:36:21 +0300

    Add Bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Creates a new procedure
+-- That adds a new correction for student
+DROP PROCEDURE IF EXISTS AddBonus;
+
+CREATE PROCEDURE AddBonus(
+    IN users_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, (SELECT id FROM projects WHERE name=project_name), score);
+END;
