00da4e387c39c1bb3520d8bb237d9dc4f31e3ca4
OteIan <oteian9@gmail.com>
2024-03-24 19:13:50 +0300

    Fix: ..

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,11 +11,11 @@ BEGIN
 
     SELECT AVG(score) INTO avg
         FROM corrections
-        WHERE user_id=user_id;
+        WHERE corrections.user_id=user_id;
     
     UPDATE users
-        SET average_score=avg
-        WHERE id=user_id;
+        SET users.average_score=avg
+        WHERE users.id=user_id;
 
 END $$
 DELIMITER ;
