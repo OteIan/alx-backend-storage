9c605f850414dc431d2a43741d545b7d44d9e326
OteIan <oteian9@gmail.com>
2024-03-24 14:33:57 +0300

    Fix: Comma

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,9 @@
 SELECT
     band_name,
     (IFNULL(split, 2022) - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock' IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+FROM
+    metal_bands
+WHERE
+    FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY
+    lifespan DESC;
