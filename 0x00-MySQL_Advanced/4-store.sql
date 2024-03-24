6db7945f32fb9d3e67f69bba566e643cbcacde9f
OteIan <oteian9@gmail.com>
2024-03-24 14:57:23 +0300

    Semicolon

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,5 @@
 -- Decreases quantity of an item after adding a new order
-DROP TRIGGER IF EXISTS update_item_number
-DELIMETER $$
+DROP TRIGGER IF EXISTS update_item_number;
 CREATE TRIGGER update_item_number
 AFTER INSERT ON orders
 FOR EACH ROW
@@ -8,5 +7,4 @@ BEGIN
     UPDATE items
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name; 
-END $$
-DELIMETER;
+END;
