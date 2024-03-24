324248c2315604c44cf0cbaa5cc1b62e36e2edfe
OteIan <oteian9@gmail.com>
2024-03-24 15:11:41 +0300

    Email validation

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Resets valid_email
+DROP TRIGGER IF NOT EXISTS reset_email;
+
+DELIMITER &&
+CREATE TRIGGER reset_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    UPDATE users
+        SET valid_email = 1
+        WHERE OLD.email != NEW.email
+END $$
+DELIMITER;
