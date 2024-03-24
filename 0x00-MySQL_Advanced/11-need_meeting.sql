3c30e385d5fbb6c003ef2edf6fb590688a454ec1
OteIan <oteian9@gmail.com>
2024-03-24 20:39:19 +0300

    Fix: 1 month

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,10 +1,11 @@
 -- Creates a view
 DROP VIEW IF EXISTS need_meeting;
 
-DELIMITER $$
-CREATE VIEW need_meeting
-SELECT name
-FROM students
-WHERE score < 80 AND (last_meeting = NULL OR last_meeting > 1)
-$$
-DELIMITER ;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+        (
+            last_meeting IS NULL
+            OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+        );
