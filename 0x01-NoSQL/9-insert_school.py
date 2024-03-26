3506ed6d893c22ce221762e548bf8ee41e6504e1
OteIan <oteian9@gmail.com>
2024-03-26 22:14:00 +0300

    Insert a new document

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 9-insert_school """
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a collection based on kwargs """
+    doc = mongo_collection.insert(kwargs)
+    return doc._id
