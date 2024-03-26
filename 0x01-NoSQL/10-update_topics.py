f42dd3bf87be72bfea1358f0e7c6b8cf0762289a
OteIan <oteian9@gmail.com>
2024-03-26 22:46:25 +0300

    Fix: key

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@ def update_topics(mongo_collection, name, topics):
     """ Changes all topics of a school doc based on the name """
     mongo_collection.update_many(
         {'name': name},
-        {'$set': {'name': topics}}
+        {'$set': {'topics': topics}}
     )
