44381c79028aa7ce6b9ea09441da418474aa0b1e
OteIan <oteian9@gmail.com>
2024-03-26 22:16:36 +0300

    Fix: Function name

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    doc = mongo_collection.insertOne(**kwargs)
+    doc = mongo_collection.insert_one(**kwargs)
     return doc._id
