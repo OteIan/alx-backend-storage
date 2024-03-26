e9437199899299993c95f24c490441388f4f96e0
OteIan <oteian9@gmail.com>
2024-03-26 22:17:15 +0300

    Fix: insert many

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    doc = mongo_collection.insert_one(**kwargs)
+    doc = mongo_collection.insert_many(**kwargs)
     return doc._id
