69b81cfa706dc236a8b90c5f02ae66e91a6bba24
OteIan <oteian9@gmail.com>
2024-03-29 22:35:37 +0300

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Cache class """
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -12,6 +12,26 @@ class Cache:
         self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Storing strings in Redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ Converts data back to the desired format """
+        data = self._redis.get(key)
+
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """ Reading strings from Redis """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ Reading ints from Redis """
+        return self.get(key, lambda x: int(x))
